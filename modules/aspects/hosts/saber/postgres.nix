3a937470893e073f9699aec0272fc27a00662cac
Paul1365972 <paul1365972@gmail.com>
2026-07-03 20:26:43 +0200

    postgres 14 -> 17

--- modules/aspects/hosts/saber/postgres.nix
+++ modules/aspects/hosts/saber/postgres.nix
@@ -2,12 +2,7 @@
 {
   den.aspects.saber.provides.postgres = {
     nixos =
-      {
-        config,
-        pkgs,
-        lib,
-        ...
-      }:
+      { pkgs, lib, ... }:
       let
         services = [
           "authentik"
@@ -16,14 +11,12 @@
           "maubot"
           "nextcloud"
         ];
-        # flip package to this after running upgrade-pg-cluster
-        newPostgres = pkgs.postgresql_17;
-        cfg = config.services.postgresql;
       in
       {
         services.postgresql = {
           enable = true;
-          package = pkgs.postgresql_14;
+          # Pinned; major-version bumps need a manual pg_upgrade (see NixOS manual).
+          package = pkgs.postgresql_17;
 
           ensureDatabases = services;
           ensureUsers = map (name: {
@@ -49,28 +42,6 @@
           startAt = "*-*-* 02:30:00";
           compression = "zstd";
         };
-
-        environment.systemPackages = [
-          (pkgs.writeShellScriptBin "upgrade-pg-cluster" ''
-            set -eux
-            systemctl stop matrix-synapse maubot traccar phpfpm-nextcloud authentik authentik-worker || true
-            systemctl stop postgresql
-
-            export NEWDATA="/var/lib/postgresql/${newPostgres.psqlSchema}"
-            export NEWBIN="${newPostgres}/bin"
-            export OLDDATA="${cfg.dataDir}"
-            export OLDBIN="${cfg.finalPackage}/bin"
-
-            install -d -m 0700 -o postgres -g postgres "$NEWDATA"
-            cd "$NEWDATA"
-            sudo -u postgres "$NEWBIN/initdb" -D "$NEWDATA" ${lib.escapeShellArgs cfg.initdbArgs}
-
-            sudo -u postgres "$NEWBIN/pg_upgrade" \
-              --old-datadir "$OLDDATA" --new-datadir "$NEWDATA" \
-              --old-bindir "$OLDBIN" --new-bindir "$NEWBIN" \
-              "$@"
-          '')
-        ];
       };
   };
 }
